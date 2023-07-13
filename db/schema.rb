42ca78f744094359167dc19e08b000b7a8be2a59
Na <na@Mac.local>
2023-07-13 18:33:09 +0900

    userのnameカラム, サインアップページにname追加

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2023_07_13_064154) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "intro"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
