d87281ef1a4f447b463ada937892fe531189387a
Na <na@Mac.local>
2023-07-15 10:34:02 +0900

    add bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,18 @@ class UsersController < ApplicationController
   def account
     @user = current_user
   end
-
-  def edit
-    @user = current_user
-  end
   
   def profile
     @user = current_user
   end
 
+
   def edit_profile 
     @user = current_user
   end
 
+  def update_profile
+    @user = current_user
+    if @user.update
+  end
 end
